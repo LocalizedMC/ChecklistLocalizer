4168f480571f0153d6e181082cc304ea1d8d3825
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-01-09 12:46:17 +0800

    clean

--- src/main/java/org/localizedmc/checklistlocalizer/ChecklistLocalizer.java
+++ src/main/java/org/localizedmc/checklistlocalizer/ChecklistLocalizer.java
@@ -4,11 +4,11 @@ import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(ChecklistModifiy.MOD_ID)
-public class ChecklistModifiy {
+@Mod(ChecklistLocalizer.MOD_ID)
+public class ChecklistLocalizer {
     public static final String MOD_ID = "checklist_localizer";
     public static final Logger LOGGER = LoggerFactory.getLogger("ChecklistLocalizer");
 
-    public ChecklistModifiy() {
+    public ChecklistLocalizer() {
     }
 }
